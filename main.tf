ce42f90d345ec17fcaa1ce4db608f1a59275acca
Aayushrestha-pixel <ayushrestha7007@gmail.com>
2026-07-06 09:33:40 +0545

    fix: add force_destroy to S3 bucket and force_delete to ECR repo to allow clean destruction

--- main.tf
+++ main.tf
@@ -54,7 +54,8 @@ data "aws_security_group" "ecs_sg" {
 
 resource "aws_ecr_repository" "app" {
 
-  name = var.app_name
+  name         = var.app_name
+  force_delete = true
 
   image_scanning_configuration {
     scan_on_push = true
@@ -285,7 +286,8 @@ resource "aws_lb_listener_rule" "app" {
 
 resource "aws_s3_bucket" "frontend" {
 
-  bucket = var.frontend_bucket_name
+  bucket        = var.frontend_bucket_name
+  force_destroy = true
 
   tags = {
     Project = "CONC"
